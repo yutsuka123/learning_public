ff1d1e8abdcf80febac7e9871f2eb3b74bbabe99
塚田豊 <ytsuka-ne2@memoad.jp>
2026-04-14 13:42:00 +0900

    260414

---
+++ c/sample.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @brief メイン関数
+ *
+ * 実行手順:
+ * cd /Users/ytsuka/mydata/src/learning_public
+ * cc c/sample.c -o c/sample
+ * ./c/sample
+ *
+ * @return プログラムの終了ステータス
+ */
+int main(void) {
+    printf("Hello World!\n");
+    return 0;
+}
